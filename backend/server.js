128c45f12c5fad03069f8f95845061326bfd5168
Vivikth Erapalli <vivikth.erapalli@gmail.com>
2024-06-17 22:13:20 +0530

    Final commit

--- backend/server.js
+++ backend/server.js
@@ -6,20 +6,26 @@ import userRoutes from  './routes/user.routes.js';
 import connectToDb from './db/connect.js';
 import cors from 'cors';
 import { app, server } from './socket/socket.js';
+import path from 'path';
 
 
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-const PORT = process.env.PORT || 3001;
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!!')
-})
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3001;
 
 app.use('/user',authRoutes);
 app.use('/message',messageRoutes);
 app.use('/allUsers',userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,() => {
     connectToDb();

7fe18b71b9fc135e20d2ddccd3d7e04494a7a5de
Vivikth Erapalli <vivikth.erapalli@gmail.com>
2024-06-18 21:29:20 +0530

    Socket url added

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173","https://chatapplication-8eox.onrender.com"],
         methods: ["GET","POST"]
     }
 })

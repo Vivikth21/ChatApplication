1f2612f825a87d94f270cfcfc8e6f76307d7617a
Vivikth Erapalli <vivikth.erapalli@gmail.com>
2024-06-20 14:14:55 +0530

    Socket Url altered

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: ["http://localhost:5173","https://chatapplication-8eox.onrender.com"],
+        origin: ["https://chatapplication-8eox.onrender.com"],
+        // origin: ["http://localhost:5173","https://chatapplication-8eox.onrender.com"],
         methods: ["GET","POST"]
     }
 })
